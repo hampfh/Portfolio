cecedc2533c61c58c06206783c1a9db649920d70
Hampus Hallkivst <hampus@hallkvist.org>
2020-01-30 07:16:53 +0100

    Further work on the console, added scrollbar and made the console scalable, also started working on the input system

--- src/components/templates/Console/Console.tsx
+++ src/components/templates/Console/Console.tsx
@@ -2,16 +2,46 @@ import React, { Component } from 'react'
 
 import styles from './Console.module.scss'
 
-export class Console extends Component {
+export class Console extends Component<{}, StateForComponent> {
+
+    constructor(props: any) {
+        super(props);
+
+        this.state = {
+            cursor: {
+
+            },
+            lines: [
+
+            ]
+        }
+    }
+
+    _handleClick(event: any) {
+        // Set focus to console
+        console.log(event.target.value)
+        console.log(event.keyCode)
+    }
+
     render() {
         return (
-            <section className={styles.console}>
-                <p className={styles.line}>
+            <section className={styles.console} onClick={this._handleClick}>
+                <div className={styles.line}>
                     <p className={styles.userPrefix}>server@user:~$</p> 
-                    sudo npm install react-router-com     gsdgdsagadgasgdasgdsagdsagasgdasddg</p>
+                    <span>sudo npm install react-router-com     gsdgdsagadgasgdasgdsagdsagasgdasddg</span></div>
+                <div className={styles.line}>
+                    <p className={styles.userPrefix}>server@user:~$</p>
+                    <input className={styles.consoleInput}></input></div>
             </section>
         )
     }
+}
+
+interface StateForComponent {
+    cursor: {
+        
+    },
+    lines: Array<string>
 }
 
 export default Console
