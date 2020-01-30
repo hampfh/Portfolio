974c1dac26edea5519c68836551827fff1a4a5f1
Hampus Hallkivst <hampus@hallkvist.org>
2020-01-30 15:54:43 +0100

    Further implementation of the console interactive functionality, expanded interface and added new method toggleCursor ...

--- src/components/templates/Console/Console.tsx
+++ src/components/templates/Console/Console.tsx
@@ -3,18 +3,22 @@ import React, { Component } from 'react'
 import styles from './Console.module.scss'
 
 export class Console extends Component<{}, StateForComponent> {
-
+    interval = 1000;
     constructor(props: any) {
         super(props);
 
         this.state = {
             cursor: {
-
+                active: false,
+                char: "█"
             },
             lines: [
 
             ]
         }
+
+        this.startInterval = this.startInterval.bind(this);
+        this.toggleCursor = this.toggleCursor.bind(this);
     }
 
     _handleClick(event: any) {
@@ -23,7 +27,25 @@ export class Console extends Component<{}, StateForComponent> {
         console.log(event.keyCode)
     }
 
+    startInterval() {
+        setInterval(this.toggleCursor, this.interval);
+    }
+
+    toggleCursor() {
+        let newState = {...this.state};
+        console.log(this.state)
+        if (newState.cursor.active) {
+            newState.lines[newState.lines.length - 1].substr(0, newState.lines[newState.lines.length - 1].length - 1);
+            newState.cursor.active = true;
+        } else {
+            newState.lines[newState.lines.length - 1] += this.state.cursor.char;
+            newState.cursor.active = false;
+        }
+        this.setState(newState);
+    }
+
     render() {
+        this.startInterval();
         return (
             <section className={styles.console} onClick={this._handleClick}>
                 <div className={styles.line}>
@@ -39,7 +61,8 @@ export class Console extends Component<{}, StateForComponent> {
 
 interface StateForComponent {
     cursor: {
-        
+        active: boolean,
+        char: string
     },
     lines: Array<string>
 }
