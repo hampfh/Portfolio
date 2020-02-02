31b532266690b8b04b8dedd4d4314ff7ce8c655e
Hampus Hallkivst <hampus@hallkvist.org>
2020-02-02 20:18:46 +0100

    Added exit animation

--- src/components/templates/Console/index.tsx
+++ src/components/templates/Console/index.tsx
@@ -7,39 +7,47 @@ import Console from 'components/templates/Console/Console'
 import { State as ConsoleState } from 'state/reducers/console'
 import { connect } from 'react-redux'
 
+import { CSSTransition } from 'react-transition-group'
 import styles from './Console.module.scss'
+import transitionStyle from './ConsoleTransitionStyle.module.scss'
 
 export class ConsoleWindow extends Component<PropsForComponent, ConsoleState> {
 
     render() {
-        if (!!!this.props.console.window.visible)
-            return null;
+        const delay = 150;
+        const style = {
+            transition: `transform ${delay}ms`
+        }
 
         return (
-            <section className={styles.consoleWindow}>
-                <Toolbar />
-                <Console interactive={true} onLoadMessage={[
-                        { 
-                            type: LineType.input, mode: OutputMode.typing, 
-                            text: [ "fetch webpage" ]
-                        },
-                        { 
-                            type: LineType.info, mode: OutputMode.default,
-                            text: [
-                                "Detecting visitor!",
-                                "Polishing shoes...",
-                                "Double check backslick...",
-                                "Ironing clothes...",
-                                "Straightening tie...",
-                                "Double check backslick...",
-                                "■■■■■■■■■■ Done!",
-                                "Hello there dear visitor!",
-                                "Welcome to the page :D"
-                            ]
-                        }
-                    ]} 
-                />
-            </section>
+            <CSSTransition in={this.props.console.window.visible} classNames={{ ...transitionStyle }} unmountOnExit timeout={delay} 
+                style={style}
+            >
+                <section className={styles.consoleWindow} style={style}>
+                    <Toolbar />
+                    <Console interactive={true} onLoadMessage={[
+                            { 
+                                type: LineType.input, mode: OutputMode.typing, 
+                                text: [ "fetch webpage" ]
+                            },
+                            { 
+                                type: LineType.info, mode: OutputMode.default,
+                                text: [
+                                    "Detecting visitor!",
+                                    "Polishing shoes...",
+                                    "Double check backslick...",
+                                    "Ironing clothes...",
+                                    "Straightening tie...",
+                                    "Double check backslick...",
+                                    "■■■■■■■■■■ Done!",
+                                    "Hello there dear visitor!",
+                                    "Welcome to the page :D"
+                                ]
+                            }
+                        ]} 
+                    />
+                </section>
+            </CSSTransition>
         )
     }
 }
