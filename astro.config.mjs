967f46d2878f54be588f1b9882885e3ec0a5ee0a
Hampus Hallkvist <hampus@hallkvist.org>
2024-01-20 13:43:07 +0100

    fix: Dependencies

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,10 +2,7 @@ import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
 import react from "@astrojs/react";
 
-import sentry from "@sentry/astro";
-import spotlightjs from "@spotlightjs/astro";
-
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind(), react(), sentry(), spotlightjs()]
+  integrations: [tailwind(), react()]
 });
