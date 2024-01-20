6d6486f6d98e6e7f0212d8f730e7bfdd41c549c9
Hampus Hallkvist <hampus@hallkvist.org>
2024-01-20 13:37:50 +0100

    fix: Update dependencies and small fixes

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,11 @@
-import { defineConfig } from "astro/config"
-import tailwind from "@astrojs/tailwind"
-import react from "@astrojs/react"
+import { defineConfig } from "astro/config";
+import tailwind from "@astrojs/tailwind";
+import react from "@astrojs/react";
+
+import sentry from "@sentry/astro";
+import spotlightjs from "@spotlightjs/astro";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind(), react()]
-})
+  integrations: [tailwind(), react(), sentry(), spotlightjs()]
+});
