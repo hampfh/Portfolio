3d1a2e37fdcec2c8ef5650483d33575a320f0e17
Hampus Hallkvist <hampus@hallkvist.org>
2025-03-29 15:31:01 +0100

    init

---
+++ src/lib/variables.ts
@@ -0,0 +1,46 @@
+// Set any item to undefined to remove it from the site or to use the default value
+
+export const GLOBAL = {
+  // Site metadata
+  username: "Hampus Hallkvist",
+  rootUrl: "https://hallkvi.st",
+  shortDescription: "Software Engineer",
+  longDescription: "Software Engineer @ Tandem Health",
+
+  // Social media links
+  githubProfile: "https://github.com/hampfh",
+  twitterProfile: "https://twitter.com/hampfh",
+  linkedinProfile: "https://www.linkedin.com/in/hampus-hallkvist/",
+
+  // Common text names used throughout the site
+  articlesName: "Blog posts",
+  projectsName: "Projects",
+  viewAll: "View All",
+
+  // Common descriptions used throughout the site
+  noArticles: "No featured articles yet.",
+  noProjects: "No featured projects yet.",
+
+  // Blog metadata
+  blogTitle: "My Thoughts & Takes",
+  blogShortDescription: "Practical wisdom, unfiltered thoughts, and hot takes.",
+  blogLongDescription:
+    "Web development, tech trends, and the occasional programming mishap.",
+
+  // Project metadata
+  projectTitle: "Projects and Code",
+  projectShortDescription:
+    "A list of my web development projects and developer tools.",
+  projectLongDescription:
+    "All of my projects, including both frontend and full-stack applications.",
+
+  // Profile image
+  profileImage: "hampus-hallkvist-image.png",
+
+  // Menu items
+  menu: {
+    home: "/",
+    projects: "/projects",
+    blog: "/blog",
+  },
+};
---
+++ src/pages/projects/github-profile.md
@@ -0,0 +1,14 @@
+---
+layout: ../../layouts/ProjectLayout.astro
+title: Dynamic Github Profile
+description: A dynamic github profile page built with Astro.
+tags: ["github", "rust", "webhooks"]
+githubUrl: https://github.com/hampfh/hampfh
+timestamp: 2025-03-29T02:39:03+00:00
+featured: true
+filename: github-profile
+---
+
+## The Details
+
+## The Features
